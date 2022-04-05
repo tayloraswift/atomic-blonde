98fafe866e60eb9a7866dfb4d74f18a29094df3e
kelvin13 <kelvin13ma@gmail.com>
2022-04-05 17:30:07 -0500

    compact test file a bit

--- testfiles/small.swift
+++ testfiles/small.swift
@@ -2,12 +2,10 @@ struct Queue<Element>:Collection
 {
     // comment https://swift.org
     var buffer:Array<Element> = [1, 2, 0b0010_1111]
-    
     #if FOO
     private 
     let b:Int = 0
     #endif
-    
     #error("foo\n\u{A0}")
     
     var _:String = "abb\(buffer[0] + 10)a"
@@ -17,14 +15,12 @@ struct Queue<Element>:Collection
     {
         "foo"
     }
-
     @discardableResult
     static 
     func + (_ lhs:inout Int, _ rhs:Int) async -> (x:Int, y:Int) 
     {
         return lhs + rhs 
     }
-    
     subscript(i:Int) -> [Element]
     {
         get 
